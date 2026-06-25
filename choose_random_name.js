d3ff2fa2f886008aae382deff2bd5e39051b6447
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-06-24 21:42:40 -0600

    choose random name exercise completed

---
+++ choose_random_name.js
@@ -0,0 +1,6 @@
+let names = ['Eddie','Daniel','Gil']
+
+let randomNumber = Math.round(Math.random()*names.length);
+console.log(randomNumber);
+console.log( `${names[randomNumber]} is going to buy lunch today!`)
+
